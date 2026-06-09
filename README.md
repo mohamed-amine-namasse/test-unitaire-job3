37afa4fe9f93881e3f66bc236db1ad723eb92ffc
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-06-09 14:11:32 +0200

    test des operations pour la calculette php avec PHPUnit

---
+++ README.md
@@ -0,0 +1,19 @@
+## On Crée un dossier `tests/` à la racine du projet
+
+## On crée un fichier `CalculatorTest.php` dans `tests/`
+
+On test l'addition
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+On test la soustraction
+![alt text](image-3.png)
+![alt text](image-4.png)
+
+On test la multiplication
+![alt text](image-5.png)
+![alt text](image-6.png)
+
+On test la division
+![alt text](image-7.png)
+![alt text](image-8.png)
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/calculator.php';
+
+class CalculatorTest extends TestCase
+{
+    public function testAddition()
+    {
+        $calculator = new Calculator();
+
+        $this->assertEquals(5, $calculator->calculate("2 + 3"));
+        $this->assertEquals(0, $calculator->calculate("-2 + 2"));
+    }
+
+    public function testSoustraction()
+    {
+        $calculator = new Calculator();
+
+        $this->assertEquals(2, $calculator->calculate("5 - 3"));
+        $this->assertEquals(0, $calculator->calculate("5 − 5"));
+    }
+
+    public function testMultiplication()
+    {
+        $calculator = new Calculator();
+
+        $this->assertEquals(15, $calculator->calculate("3 * 5"));
+        $this->assertEquals(15, $calculator->calculate("3 × 5"));
+    }
+
+    public function testDivision()
+    {
+        $calculator = new Calculator();
+
+        $this->assertEquals(2, $calculator->calculate("6 / 3"));
+        $this->assertEquals(2, $calculator->calculate("6 ÷ 3"));
+    }
+}
