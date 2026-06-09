aa3820a55cfd7f24c834ac6fe7607bb35f83a4df
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-06-09 15:10:25 +0200

    correction des captures pour calculatrice php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -29,12 +29,4 @@ class CalculatorTest extends TestCase
         $this->assertEquals(15, $calculator->calculate("3 * 5"));
         $this->assertEquals(15, $calculator->calculate("3 × 5"));
     }
-
-    public function testDivision()
-    {
-        $calculator = new Calculator();
-
-        $this->assertEquals(2, $calculator->calculate("6 / 3"));
-        $this->assertEquals(2, $calculator->calculate("6 ÷ 3"));
-    }
 }
