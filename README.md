6a5aa1ea25188f9bec184444669241f46149e08e
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-06-09 14:56:42 +0200

    test des operations pour la calculatrice JS avec jest

--- README.md
+++ README.md
@@ -17,3 +17,26 @@ On test la multiplication
 On test la division
 ![alt text](image-7.png)
 ![alt text](image-8.png)
+
+## On crée un fichier `calculator.test.js` dans `tests/`
+
+On test l'addition
+![alt text](image-9.png)
+
+On test la soustraction
+![alt text](image-10.png)
+
+On test la multiplication
+![alt text](image-11.png)
+
+On test la division
+![alt text](image-12.png)
+
+On test les priorités des opérateurs
+![alt text](image-13.png)
+
+On test Les parenthèses ((2+3)\*4)
+![alt text](image-14.png)
+
+On test le comportement en cas d'expression invalide (2+bad)
+![alt text](image-15.png)
---
+++ tests/calculator.test.js
@@ -0,0 +1,44 @@
+const { calculate } = require("../calculator");
+
+describe("Tests de la calculatrice avec Jest", () => {
+  // L’addition
+  test("calculer correctement une addition", () => {
+    expect(calculate("2 + 3")).toBe(5);
+    expect(calculate("10 + 20 + 30")).toBe(60);
+  });
+
+  // La soustraction
+  test("calculer correctement une soustraction", () => {
+    expect(calculate("5 - 3")).toBe(2);
+    expect(calculate("10 - 15")).toBe(-5);
+  });
+
+  //  La multiplication
+  test("calculer correctement une multiplication", () => {
+    expect(calculate("3 * 4")).toBe(12);
+    expect(calculate("0 * 5")).toBe(0);
+  });
+
+  // La division
+  test("calculer correctement une division", () => {
+    expect(calculate("6 / 3")).toBe(2);
+    expect(calculate("5 / 2")).toBe(2.5);
+  });
+
+  //  Les priorités (2+3*4)
+  test("respecter les priorités des opérateurs (multiplication avant addition)", () => {
+    expect(calculate("2+3*4")).toBe(14);
+  });
+
+  // Les parenthèses ((2+3)*4)
+  test("forcer la priorité avec des parenthèses", () => {
+    expect(calculate("(2+3)*4")).toBe(20);
+  });
+
+  //  Le comportement en cas d'expression invalide (2+bad)
+  test("afficher une erreur en cas d'expression invalide ", () => {
+    expect(() => {
+      calculate("2+bad");
+    }).toThrow("Expression invalide");
+  });
+});
